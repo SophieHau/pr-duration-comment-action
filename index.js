65ed7288b0c44184caf0a129ac0bb0f174c26fdf
sophie <s.hautcoeur@gmail.com>
2020-05-29 16:31:03 +0300

    adding a console log

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+
 try {
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     const comment = JSON.stringify(github.context.payload.comment.body, undefined, 2)
     const pr = JSON.stringify(github.context.payload.pull_request.body)
     console.log(`The event comment: ${comment} & the event pr: ${pr}`);
-  } catch (error) {
+    console.log(time)
+  }  catch (error) {
     core.setFailed(error.message);
   }
