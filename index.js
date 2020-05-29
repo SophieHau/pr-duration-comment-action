a7a0575c606647dc6e79e23652bd4db95f8bbbd2
sophie <s.hautcoeur@gmail.com>
2020-05-29 16:48:49 +0300

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     const comment = JSON.stringify(github.context.payload.comment.body, undefined, 2)
-    const pr = JSON.stringify(github.context.payload.pull_request.body)
+    const pr = JSON.stringify(github.context.payload.pull_request.body, undefined, 2)
     console.log(`The event comment: ${comment} & the event pr: ${pr}`);
     console.log(time)
   } catch (error) {
