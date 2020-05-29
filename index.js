820da173d2c57f97279c4df960d25a9ab6d2f5f8
sophie <s.hautcoeur@gmail.com>
2020-05-29 16:34:27 +0300

    added console logs

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ try {
     const comment = JSON.stringify(github.context.payload.comment.body, undefined, 2)
     const pr = JSON.stringify(github.context.payload.pull_request.body)
     console.log(`The event comment: ${comment} & the event pr: ${pr}`);
+<<<<<<< HEAD
     console.log(time)
   }  catch (error) {
+=======
+  } catch (error) {
+>>>>>>> fac9ac0f44daeccd67278010be1e96f3e847e8a8
     core.setFailed(error.message);
   }
