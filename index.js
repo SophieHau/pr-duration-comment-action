b8086cf271eb71115f1851d5238408a7f1056fde
sophie <s.hautcoeur@gmail.com>
2020-05-29 18:45:25 +0300

    adding comment try5

--- index.js
+++ index.js
@@ -10,21 +10,20 @@ async function run() {
       const repository = process.env.GITHUB_REPOSITORY;
       const ref = process.env.GITHUB_REF;
       const pr_ref = ref.split("/")
-      console.log(pr_ref)
+      console.log(process.env)
     
       const repo = repository.split("/");
       core.debug(`repository: ${repository}`);
   
-      console.log(repo)
       const octokit = new github.GitHub(inputs.token);
-      console.log(octokit.pulls)
   
       // Create a comment
       const { data: comment } = await octokit.pulls.createComment({
         owner: repo[0],
         repo: repo[1],
-        pull_number: pr_ref[-1],
-        body: "this is the message"
+        pull_number: pr_ref[2],
+        body: "this is the message",
+        commit_id: ''
       });
       core.info(
         `Created comment id '${comment.id}'.`
