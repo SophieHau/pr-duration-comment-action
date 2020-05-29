f2b4b2928c3771e21fd5c5d0773d9a97d96cf04b
sophie <s.hautcoeur@gmail.com>
2020-05-29 17:31:26 +0300

    adding comment try1

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
+const { inspect } = require("util");
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 
-try {
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event comment: ${payload}`);
-    console.log(time)
-  } catch (error) {
-    core.setFailed(error.message);
+async function run() {
+    try {
+      const inputs = {
+        token: core.getInput("token"),
+        repository: core.getInput("repository"),
+        issueNumber: core.getInput("issue-number"),
+        commentId: core.getInput("comment-id"),
+        body: core.getInput("body"),
+      };
+      console.log(inputs)
+      console.log(process.env.GITHUB_REPOSITORY)
+      core.debug(`Inputs: ${inspect(inputs)}`);
+  
+      const repository = inputs.repository
+        ? inputs.repository
+        : process.env.GITHUB_REPOSITORY;
+      const repo = repository.split("/");
+      core.debug(`repository: ${repository}`);
+  
+  
+      const octokit = new github.GitHub(inputs.token);
+  
+     if (inputs.issueNumber) {
+        // Create a comment
+        if (!inputs.body) {
+          core.setFailed("Missing comment 'body'.");
+          return;
+        }
+        const { data: comment } = await octokit.issues.createComment({
+          owner: repo[0],
+          repo: repo[1],
+          issue_number: inputs.issueNumber,
+          body: inputs.body,
+        });
+        core.info(
+          `Created comment id '${comment.id}' on issue '${inputs.issueNumber}'.`
+        );
+        core.setOutput("comment-id", comment.id);
+
+      } else {
+        core.setFailed("Missing either 'issue-number' or 'comment-id'.");
+        return;
+      }
+    } catch (error) {
+      core.debug(inspect(error));
+      core.setFailed(error.message);
+    }
   }
+  
+  run();
