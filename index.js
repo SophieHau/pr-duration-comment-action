1146a39208ba49d371a72f390ff013180a74cfdf
sophie <s.hautcoeur@gmail.com>
2020-05-29 19:49:19 +0300

    adding comment try12

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ async function run() {
         token: core.getInput("token"),
       };
       const repository = process.env.GITHUB_REPOSITORY;
-      const ref = process.env.GITHUB_REF;
-      const pr_ref = ref.split("/")
+      // const ref = process.env.GITHUB_REF;
+      // const pr_ref = ref.split("/")
 
       const time = (new Date()).toTimeString();
       
@@ -16,16 +16,18 @@ async function run() {
       core.debug(`repository: ${repository}`);
   
       const octokit = new github.GitHub(inputs.token);
-      console.log("listevents:", octokit.issues.listEvents())
-      console.log("meta: ", octokit.meta.get())
-      console.log("issues get: ", octokit.issues.get())
-      console.log("issues milestone: ", octokit.issues.getMilestone())
+      issue_number = github.context.issue.number
+      console.log("issue number: ", issue_number)
+      console.log("listevents:",await octokit.issues.listEvents(issue_number))
+      console.log("meta: ", await octokit.meta.get(issue_number))
+      console.log("issues get: ", await octokit.issues.get(issue_number))
+      console.log("issues milestone: ", await octokit.issues.getMilestone(issue_number))
   
       // Create a comment
       const { data: comment } = await octokit.issues.createComment({
         owner: repo[0],
         repo: repo[1],
-        issue_number: pr_ref[2],
+        issue_number: issue_number,
         body: `this is the message at ${time}`,
       });
       core.info(
