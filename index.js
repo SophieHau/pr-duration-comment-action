066c6aafa1c812940f44581f1280d9def912b66c
sophie <s.hautcoeur@gmail.com>
2020-05-29 17:44:51 +0300

    adding comment try2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { inspect } = require("util");
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -7,45 +6,30 @@ async function run() {
     try {
       const inputs = {
         token: core.getInput("token"),
-        repository: core.getInput("repository"),
-        issueNumber: core.getInput("issue-number"),
-        commentId: core.getInput("comment-id"),
         body: core.getInput("body"),
       };
       console.log(inputs)
-      console.log(process.env.GITHUB_REPOSITORY)
-      core.debug(`Inputs: ${inspect(inputs)}`);
-  
-      const repository = inputs.repository
-        ? inputs.repository
-        : process.env.GITHUB_REPOSITORY;
+      console.log(process.env)  
+      const repository = process.env.GITHUB_REPOSITORY;
+    
       const repo = repository.split("/");
       core.debug(`repository: ${repository}`);
   
   
       const octokit = new github.GitHub(inputs.token);
+      console.log(octokit)
   
-     if (inputs.issueNumber) {
-        // Create a comment
-        if (!inputs.body) {
-          core.setFailed("Missing comment 'body'.");
-          return;
-        }
-        const { data: comment } = await octokit.issues.createComment({
-          owner: repo[0],
-          repo: repo[1],
-          issue_number: inputs.issueNumber,
-          body: inputs.body,
-        });
-        core.info(
-          `Created comment id '${comment.id}' on issue '${inputs.issueNumber}'.`
-        );
-        core.setOutput("comment-id", comment.id);
+      // Create a comment
+      const { data: comment } = await octokit.issues.createComment({
+        owner: repo[0],
+        repo: repo[1],
+        body: inputs.body,
+      });
+      core.info(
+        `Created comment id '${comment.id}' on issue '${inputs.issueNumber}'.`
+      );
+      core.setOutput("comment-id", comment.id);
 
-      } else {
-        core.setFailed("Missing either 'issue-number' or 'comment-id'.");
-        return;
-      }
     } catch (error) {
       core.debug(inspect(error));
       core.setFailed(error.message);
