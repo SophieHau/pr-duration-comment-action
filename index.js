bd3f30fe0bce9f10755cf64f20b3b0a444d09258
Sophie <47497299+SophieHau@users.noreply.github.com>
2020-05-29 16:24:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const github = require('@actions/github');
 try {
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const comment = JSON.stringify(github.context.payload.comment.body, undefined, 2)
+    const pr = JSON.stringify(github.context.payload.pull_request.body)
+    console.log(`The event comment: ${comment} & the event pr: ${pr}`);
   } catch (error) {
     core.setFailed(error.message);
   }
